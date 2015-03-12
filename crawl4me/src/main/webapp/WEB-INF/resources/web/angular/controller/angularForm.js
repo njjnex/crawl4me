e8bb04e7ff74a142bbeea7af11e1f6825827a4cc
Noxx - netbook <njjnex@gmail.com>
2015-03-12 20:18:55 +0400

    12.03 design polish

--- crawl4me/src/main/webapp/WEB-INF/resources/web/angular/controller/angularForm.js
+++ crawl4me/src/main/webapp/WEB-INF/resources/web/angular/controller/angularForm.js
@@ -211,9 +211,9 @@ app
 		            		var domPattern = /^[<]\w{1,}\W\w{1,}[=]["][a-z\sA-Z-0-9]{1,30}["][>]$/;
 		            		if(!domPattern.test($scope.domRules[i].value)){
 		            			expErrors++;
-		            			 
+		            			 console.log("error");
 		            			 var modalInstance = $modal.open({
-		            				  template: '<div style="background:yellow; margin="5%;"><h2 style="text-align: center; color: red;">Incorrect or empty value in extract data field.</h2> <ins> Field: <strong>'+ $scope.domRules[i].key + '</strong> position in list: <strong>' + ++i  +'</strong></ins><br> Please specify correct selector.<br> It should looks like: &lt;div class="my class"&gt;<br> Visit <a href="/howTo">HowTo page</a> for more information.</div>',
+		            				  template: '<div style="background:Coral ; margin="10%;"><h2 style="text-align: center; color: DarkCyan ;">Incorrect or empty value in extract data field.</h2> <ins> Field: <strong>'+ $scope.domRules[i].key + '</strong> position in list: <strong>' + ++i  +'</strong></ins><br> Please specify correct selector.<br> It should looks like: &lt;div class="my class"&gt;<br> Visit <a href="/howTo">HowTo page</a> for more information.</div>',
 				            	      size: 'sm',
 				            	 });
 		            			
